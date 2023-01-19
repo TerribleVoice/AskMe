1a830c335699128047e38bd783d91e4ec6275d78
Михаил Штефан <m.shtefan@bars.group>
2023-01-19 15:26:19 +0500

    написал класс Result

---
+++ Backend/AskMe.Core/Models/Result.cs
@@ -0,0 +1,34 @@
+﻿namespace AskMe.Core.Models;
+
+public class Result
+{
+    private Result(bool isSuccess, string? msg = null)
+    {
+        IsSuccess = isSuccess;
+        ErrorMsg = msg;
+    }
+
+    public bool IsSuccess { get; set; }
+    public bool IsFailure => !IsSuccess;
+    public string? ErrorMsg { get; set; }
+
+    public static Result Success() => new(true);
+    public static Result Fail(string msg) => new(false, msg);
+}
+
+public class Result<T>
+{
+    public T? Value { get; set; }
+    public bool IsSuccess { get; set; }
+    public bool IsFailure => !IsSuccess;
+    public string? ErrorMsg { get; set; }
+    private Result(bool isSuccess, T? value, string? msg = null)
+    {
+        Value = value;
+        IsSuccess = isSuccess;
+        ErrorMsg = msg;
+    }
+
+    public static Result<T> Success(T value) => new(true, value);
+    public static Result<T> Fail(string msg) => new(false, default, msg);
+}
