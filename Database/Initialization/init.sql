bbefd9e006d6c1a92e2ab428daef6161eb5ce0f6
Михаил Штефан <m.shtefan@bars.group>
2023-01-20 00:04:38 +0500

    создал таблицу для постов в бд

--- Database/Initialization/init.sql
+++ Database/Initialization/init.sql
@@ -7,3 +7,18 @@ CREATE TABLE public.users
     is_author   bool    NOT NULL,
     CONSTRAINT users_pk PRIMARY KEY (id)
 );
+
+CREATE TABLE public.posts (
+    id uuid NOT NULL,
+    author_id uuid NOT NULL,
+    subscription_id uuid NOT NULL,
+    contnet text NOT NULL,
+    created_at timestamp with time zone NOT NULL,
+    price integer
+);
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_pk PRIMARY KEY (id);
+
+ALTER TABLE ONLY public.posts
+    ADD CONSTRAINT posts_fk FOREIGN KEY (author_id) REFERENCES public.users(id) ON DELETE SET NULL;
---
+++ Database/Initialization/users_202301192355.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.users (id,login,email,"password",is_author) VALUES
+	 ('a8681dd5-7a5a-4ae5-8237-583edfb2eedb','test','test@ya.ru','123',true),
+	 ('3560787e-4830-4fba-9673-9f47c9a3f8c8','reader','reader@ya.ru','123',false);
