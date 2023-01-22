4f7e9e9f813331db0bd7b083c2a76e62006a5146
Михаил Штефан <m.shtefan@bars.group>
2023-01-23 01:58:58 +0500

    поправил опечатку в названии колонки и перенес создание записей в init.sql

--- Database/Initialization/init.sql
+++ Database/Initialization/init.sql
@@ -12,7 +12,7 @@ CREATE TABLE public.posts (
     id uuid NOT NULL,
     author_id uuid NOT NULL,
     subscription_id uuid NOT NULL,
-    contnet text NOT NULL,
+    content text NOT NULL,
     created_at timestamp with time zone NOT NULL,
     price integer
 );
@@ -22,3 +22,11 @@ ALTER TABLE ONLY public.posts
 
 ALTER TABLE ONLY public.posts
     ADD CONSTRAINT posts_fk FOREIGN KEY (author_id) REFERENCES public.users(id) ON DELETE SET NULL;
+
+
+INSERT INTO public.users (id,login,email,"password",is_author) VALUES
+    ('a8681dd5-7a5a-4ae5-8237-583edfb2eedb','test','test@ya.ru','123',true),
+    ('3560787e-4830-4fba-9673-9f47c9a3f8c8','reader','reader@ya.ru','123',false);
+
+INSERT INTO public.posts (id,author_id,subscription_id,contnet,created_at,price) VALUES
+    ('fbad2687-42f4-4cb3-8534-61c8936ec4a8','a8681dd5-7a5a-4ae5-8237-583edfb2eedb','4dcde46f-6f38-4400-bd1e-e495f148afdd','Всем привет, это первый пост автоматически сгенерированный при создании БД. Если вы его видите, то подключение к бд работает хорошо','2023-01-19 23:00:00+05',NULL);
